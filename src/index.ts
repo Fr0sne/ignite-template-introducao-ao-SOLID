a7597b0d946c337aad5f7f28e5489315d18c234f
Gabriel Davi <gabriel16david@hotmail.com>
2022-07-13 01:04:41 -0300

    feat:swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-
+import swagger from "swagger-ui-express";
 import { usersRoutes } from "./routes/users.routes";
+import yaml from "yamljs";
 
+const swaggerdoc = yaml.load("./swagger.yaml");
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerdoc));
 app.use("/users", usersRoutes);
-
 export { app };
